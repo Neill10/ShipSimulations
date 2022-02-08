76c976200e2a47de965482a02b9f68c7fb164717
Neill10 <neill10@nycstudents.net>
2022-02-08 17:38:36 -0500

    Almost finished with the Address class. Did not have condition to check if zipcode length has to be 5.

--- Ship Simulations/src/Simulation.java
+++ Ship Simulations/src/Simulation.java
@@ -1,5 +1,15 @@
 public class Simulation {
     public static void main(String[] args) {
-
+        Address test = new Address("1313 Mockingbird Lane, Springfield, MO 65123");
+        System.out.println(test);
+        System.out.println(test.getStreet());
+        Address test2 = new Address(test);
+        System.out.println(test2);
+        System.out.println(test2.getStreet());
+        Address test3 = new Address("1313 Mockingbird Lane Apt 6A, Springfield, MO 65123");
+        System.out.println("-------------------------");
+        System.out.println(test.compare(test2));
+        System.out.println(test.compare(test3));
+        System.out.println(test2.compare(test3));
     }
 }
